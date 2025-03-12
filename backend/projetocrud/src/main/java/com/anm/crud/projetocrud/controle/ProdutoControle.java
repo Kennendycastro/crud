90331a7c9611a2d81cc3e8b6ac904a92cb99ba05
Kennendycastro <kennendycastro3@gmail.com>
2025-03-12 10:23:42 -0300

    Criação da rota /listar

--- backend/projetocrud/src/main/java/com/anm/crud/projetocrud/controle/ProdutoControle.java
+++ backend/projetocrud/src/main/java/com/anm/crud/projetocrud/controle/ProdutoControle.java
@@ -1,12 +1,25 @@
 package com.anm.crud.projetocrud.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.anm.crud.projetocrud.modelo.ProdutoModelo;
+import com.anm.crud.projetocrud.servico.ProdutoServico;
 
 
 
 @RestController
 public class ProdutoControle{
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+
+        return ps.listar();
+    }
 
     @GetMapping("/")
     public String rota() {
