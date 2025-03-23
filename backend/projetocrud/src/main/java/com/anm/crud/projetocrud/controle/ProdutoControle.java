8c61c6e590f7fc205ac115ee1922cc6971489325
Kennendycastro <kennendycastro3@gmail.com>
2025-03-23 18:28:52 -0300

    Finalização do CRUD da API

--- backend/projetocrud/src/main/java/com/anm/crud/projetocrud/controle/ProdutoControle.java
+++ backend/projetocrud/src/main/java/com/anm/crud/projetocrud/controle/ProdutoControle.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +25,20 @@ public class ProdutoControle{
 
     private ProdutoServico ps;
 
+    @DeleteMapping("/remover/{codigo}")
+
+    public ResponseEntity<RespostaModelo> remover(@PathVariable Long codigo){
+
+        return ps.remover(codigo);
+    }
+
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastro(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
         return ps.cadastrarAlterar(pm, "alterar");
     }
 
