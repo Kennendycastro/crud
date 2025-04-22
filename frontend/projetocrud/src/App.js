1b7fab6d796f70b90fb73e39ccf9321dc1ec7a01
Kennendycastro <kennendycastro3@gmail.com>
2025-04-22 08:10:02 -0300

    Criação da classe formulario

--- frontend/projetocrud/src/App.js
+++ frontend/projetocrud/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import {userState} from 'react';
 import './App.css';
-
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[btnCadastrar] = userState(false);
+
+
+  return(
+    <div>
+      <Formulario botao = {btnCadastrar}/>
+      <Tabela/>
     </div>
-  );
+  )
 }
 
 export default App;
--- frontend/projetocrud/src/fromulario.js
+++
@@ -1,8 +0,0 @@
-function Fromulario() {
-    return (
-        <h1>Fromulario</h1>
-    )
-    
-}
-
-export default Fromulario;
