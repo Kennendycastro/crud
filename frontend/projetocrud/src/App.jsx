924a54556716bbddcfe54d27e1a705d2f788d5f9
Kennendycastro <kennendycastro3@gmail.com>
2025-04-29 11:16:49 -0300

    terminado o codigo de App.js e Formulario

--- frontend/projetocrud/src/App.jsx
+++ frontend/projetocrud/src/App.jsx
@@ -1,17 +1,26 @@
-import {userState} from 'react';
-import './App.css';
-import Formulario from './Formulario';
-import Tabela from './Tabela';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Formulario from "./Formulario";
+import Tabela from "./Tabela";
+
 function App() {
-  const[btnCadastrar] = userState(false);
+  const [btnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
 
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => setProdutos(retorno_convertido));
+  }, []);
 
-  return(
+  return (
     <div>
-      <Formulario botao = {btnCadastrar}/>
-      <Tabela/>
+      <p>{JSON.stringify(produtos)}</p>
+      <Formulario botao={btnCadastrar} />
+      <Tabela />
     </div>
-  )
+  );
 }
 
 export default App;
