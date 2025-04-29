f1bd280beff7d4cf4ae47b9e83a01b0915bde603
Kennendycastro <kennendycastro3@gmail.com>
2025-04-29 18:08:21 -0300

    Estilização da pagina de cadrastro de produto

--- frontend/projetocrud/src/App.jsx
+++ frontend/projetocrud/src/App.jsx
@@ -1,26 +1,29 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import Formulario from "./Formulario";
-import Tabela from "./Tabela";
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
-  const [btnCadastrar] = useState(true);
-  const [produtos, setProdutos] = useState([]);
+
+const[btnCadastrar] =useState(true);
+const[produtos, setProdutos] =useState([]);
+
 
   //UseEffect
   useEffect(() => {
     fetch("http://localhost:8080/listar")
-      .then((retorno) => retorno.json())
-      .then((retorno_convertido) => setProdutos(retorno_convertido));
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
   }, []);
 
-  return (
-    <div>
-      <p>{JSON.stringify(produtos)}</p>
-      <Formulario botao={btnCadastrar} />
-      <Tabela />
-    </div>
-  );
+
+return (    
+      <div>        
+        <Formulario botao={btnCadastrar}/>
+        <Tabela vetor={produtos}/>
+      </div>
+  )
 }
 
 export default App;
--- frontend/projetocrud/src/Formulario.jsx
+++ frontend/projetocrud/src/Formulario.jsx
@@ -1,20 +1,18 @@
-function Formulario(botao){
-    return (
+function Formulario({botao}){
+    return(
         <form>
-            <h1>Formulário</h1>
-
-            <input className="form-control" type="text" placeholder="Nome"/>
-            <input className="form-control" type="text" placeholder="Marca"/>
-
+            <h2>Sistema Gestor de Produtos</h2>
+            <input className="form-control" type="text" placeholder="Nome"></input>
+            <input className="form-control" type="text" placeholder="Marca"></input>
             {
-                botao
+               botao
                 ?
-                <input className="btn btn-success" type="button" value="Cadastrar"/>
+                <input className="btn btn-success" type="button" value="Cadastrar" />
                 :
                 <div>
-                <input className="btn btn-dark" type="button" value="Alterar"/>
-                <input className="btn btn-danger" type="button" value="Remover"/>
-                <input className="btn btn-warning" type="button" value="Cancelar"/>
+                    <input className="btn btn-warning" type="button" value="Cancelar" />
+                    <input className="btn btn-primary" type="button" value="Alterar" />
+                    <input className="btn btn-danger"  type="button" value="Remover" />
                 </div>
             }
         </form>
